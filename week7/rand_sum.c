f1a7a5aef5da1c86da511aad4b97a04120adc83f
JeongSeoJin <pysuk88@gmail.com>
2024-10-18 14:59:15 +0900

    Add: week6 files

--- week7/rand_sum.c
+++ week7/rand_sum.c
@@ -3,8 +3,20 @@
 
 int main(void) {
     srand(time(NULL)); // rand함수는 무작위 값을 출력하지만 시드값으로 time이 없다면 여러번 실행했을 떄 결과값이 같을 수 있다.
+    int x, y, answer, i;
     
     for (int i = 0; i < 10; i ++) {
-        printf("%d\n", rand());
+        // printf("%d\n", rand());
+        x = rand() % 10;
+        y = rand() % 10;
+        printf("%d + %d = ", x, y);
+        scanf("%d", &answer);
+        if(x + y == answer) {
+            printf("correct!\n");
+        } 
+        else {
+            printf("wrong!\n");
+        }
     }
+    return 0;
 }
